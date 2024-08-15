3d226cfeba0aa3ab7cf7fe11a80757785235d362
Musfique55 <mdmusfiquepatwary@gmail.com>
2024-08-15 12:09:36 +0600

    initial server setup done

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/',(req,res) => {
+    res.send('running')
+})
+app.listen(port);
